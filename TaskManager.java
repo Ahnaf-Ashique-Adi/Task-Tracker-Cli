c7ec5e2ea16066f062f4a981b431cdaaf0e6f88e
Ahnaf-Ashique-Adi <ahnafashiqueadi@gmail.com>
2025-09-26 17:59:53 +0600

    added the file writer for making a json file

--- TaskManager.java
+++ TaskManager.java
@@ -1,12 +1,41 @@
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 class TaskManager
 {
     //considering JSON as Array 
     private Task[] tasks;
     
-    // ei variable tai Task type array ke size dibe 
+    // ei variable ta comparision e help korbe  
     private int size;
-    
-    private 
+    //file create 
+    private File file ;
+
+    TaskManager()
+    {
+        this.tasks=new Task[15];
+        this.size=0;
+        this.file=new File("tasks.json");
+        
+        try
+        {
+            if(!file.exists())//mane file nai taile
+            {
+                file.createNewFile();
+                FileWriter writer=new FileWriter(file);
+                writer.write("[]");//JSON Start and ENd 
+                writer.close();
+            }
+
+        }
+        catch(IOException e)
+        {
+            System.out.println("Error"+e.getMessage());
+        }
+
+
+    }
+
+
     
 }
