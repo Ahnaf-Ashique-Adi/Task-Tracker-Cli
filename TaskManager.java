0ae4ea150ceb3e98ab2bf5ccd0b91bd9e4dfa625
Ahnaf-Ashique-Adi <ahnafashiqueadi@gmail.com>
2025-09-26 17:07:26 +0600

    1st trial

--- TaskManager.java
+++ TaskManager.java
@@ -1,4 +1,12 @@
+import java.io.File;
 class TaskManager
 {
+    //considering JSON as Array 
+    private Task[] tasks;
+    
+    // ei variable tai Task type array ke size dibe 
+    private int size;
+    
+    private 
     
 }
